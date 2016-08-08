73f39355eaf3d8f4ec002b2822be9c4d3b600e55
Le Minh An <an.leminh@ptmobilegroup.com>
2016-08-08 18:34:34 +0700

    Add Music
    Add Music

--- app/src/main/java/leminhan/entertainment/MusicActivity.java
+++ app/src/main/java/leminhan/entertainment/MusicActivity.java
@@ -1,33 +1,18 @@
 package leminhan.entertainment;
 
-import android.graphics.Color;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MusicActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Inflate the mainLayout
         setContentView(R.layout.activity_music);
-        initView();
-    }
-    private void initView() {
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        changeStatusBarColor();
+
     }
 
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
 }
